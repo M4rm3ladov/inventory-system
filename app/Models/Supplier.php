b6793eb4829a7e6fe93ab067cbbb36a2eb749991
Renier Antonio <renier.antonio20@gmail.com>
2024-04-27 21:35:36 +0800

    added fillable and scope search for supplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
+
+    public function scopeSearch($query, $value)
+    {
+        $query->where('name', 'like', "%{$value}%")
+            ->orWhere('address', 'like', "%{$value}%")
+            ->orWhere('email', 'like', "%{$value}%")
+            ->orWhere('phone', 'like', "%{$value}%");
+    }
 }
