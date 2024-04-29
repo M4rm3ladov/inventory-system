9b8eebc4a7ab25a9fd66375cf55c707816bfc7cc
Renier Antonio <renier.antonio20@gmail.com>
2024-04-29 17:18:47 +0800

    added files for services

---
+++ app/Livewire/AllServices.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AllServices extends Component
+{
+    public function render()
+    {
+        return view('livewire.all-services');
+    }
+}
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    protected $fillable = [
+        'code',
+        'description',
+        'image',
+        'price_A',
+        'price_B',
+    ];
+
+    public function service_categories() {
+        return $this->hasOne(ServiceCategory::class);
+    }
+
+    public function scopeSearch($query, $value)
+    {
+        $query->where('name', 'like', "%{$value}%");
+    }
 }
